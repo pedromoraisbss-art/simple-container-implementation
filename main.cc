7384acc23eacc571a8b24c4b13b874a18ce90b84
Pedro Morais <pedromoraisbss@gmail.com>
2026-09-10 17:00:16 -0300

    Refactor container to continuos_container with template
    add restrict type and pop_back

--- main.cc
+++ main.cc
@@ -1,96 +1,93 @@
-#include <algorithm>
-#include <cassert>
 #include <cstddef>
 #include <iostream>
-#include <iterator>
+#include <memory.h>
 #include <memory>
-#include <string>
 #include <type_traits>
 
-struct node{
+template<class t, class allocator = std::allocator<t>>
+struct continuos_container{
+    using type = t;
+    using simple_stack = allocator;
+    bool borrow = true;
+    constexpr int BorrowType(){
+        if(!std::is_integral<type>()){
+            borrow = false;
+            std::cout << "err";
 
-};
-
-template<typename dynamic_type, typename Allocator = std::allocator<dynamic_type>>
-struct container :  node{
-    using type = dynamic_type;
-    using alloc_type = Allocator;
-    using ptr_obj = dynamic_type *;
-    using ref_obj = dynamic_type &;
-
-    type *data_;
-    std::size_t size_;
-    std::size_t capacity_;
-
+        }
+        return borrow;
+    }
     struct iterator{
-        using iterator_category_acess = std::random_access_iterator_tag;
-        using diference = std::ptrdiff_t;
-        ptr_obj ptr_;
-        iterator(ptr_obj ptr) : ptr_(ptr){
+        type *dyn;
+        iterator(type *dyn_) : dyn(dyn_){}
 
+        type& operator*() const{
+            return  *dyn;
         }
-        ref_obj operator*() const{
-            return  *ptr_;
-        }
-        ptr_obj operator->() const{
-            return ptr_;
+        type* operator->() const{
+            return dyn;
         }
         iterator operator++(){
-            ++ptr_;
+            ++dyn;
             return *this;
         }
         bool operator==(const iterator &other_obj) const{
-            return ptr_ == other_obj.ptr_;
+            return dyn == other_obj.dyn;
         }
         bool operator !=(const iterator &other_obj) const{
-            return ptr_ != other_obj.ptr_;
+            return dyn != other_obj.dyn;
         }
     };
+    type *heap;
+    size_t size;
+    size_t capacity;
 
-    container() : data_(nullptr), size_(0), capacity_(0) {
-        static_assert(!std::is_void<dynamic_type>());
-    }
-    container(const container &other): size_(other.size_), capacity_(other.capacity_){
-        data_ = alloc_type().allocate(capacity_);
-        if(alloc_type().address(data_) ==  nullptr){std::cerr << "nullprt\n";}
-        std::copy(other.data_, other.data_ + size_, data_);
-    }
-    ~container(){
-        alloc_type().deallocate(data_, capacity_);
-        std::cout << "dealloc container";
+    continuos_container() : heap(nullptr),
+        size(0),
+        capacity(0){}
+    continuos_container(const continuos_container &other) : size(other.size),
+        capacity(other.capacity){
+        heap = simple_stack().allocate(capacity);
     }
 
-    void push_back(const dynamic_type &value){
-        if(size_ == capacity_){
-            size_t new_alloc = capacity_  ? 2 * capacity_ : 1;
-            dynamic_type *new_data = alloc_type().allocate(new_alloc);
-            std::copy(data_, data_ + size_, new_data);
-            alloc_type().deallocate(data_, capacity_);
-            data_ = new_data;
-            capacity_ = new_alloc;
+    ~continuos_container(){
+        simple_stack().deallocate(heap, capacity);
+    }
+
+    void push_back(const type &rvalue){
+        if(size==capacity){
+            size_t reallocate = capacity ? 2*capacity : 1;
+            type *newObj =  simple_stack().allocate(reallocate);
+            std::copy(heap, heap + size, newObj);
+            simple_stack().deallocate(heap, capacity);
+            heap = newObj;
+            capacity = reallocate;
         }
-        data_[size_++] = value;
+        if(BorrowType())
+            heap[size++] = rvalue;
     }
-    size_t size() const{
-        return size_;
+    void pop_back(){
+        if(size > 0){
+            --size;
+        }
+    }
+    size_t size_() const{
+        return size;
     }
     iterator begin(){
-        return iterator(data_);
+        return iterator(heap);
     }
     iterator end(){
-        return iterator(data_ + size_);
+        return iterator(heap + size);
     }
-
 };
 
 int main(){
-    container<int> NewContainer;
-    NewContainer.push_back(1);
-    NewContainer.push_back(2);
-    NewContainer.push_back(3);
-    NewContainer.push_back(4);
-    for(const auto i : NewContainer){
-        std::cout << i << '\n';
+    continuos_container<int> box;
+    box.push_back(1);
+    box.push_back(2);
+    for(const auto &r : box){
+        std::cout << r;
     }
     return 0;
 }
